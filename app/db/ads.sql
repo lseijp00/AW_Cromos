3332f1d14d0dcf6a993b0c59d3729deb47a21f99
msperl00 <msperl00@estudiantes.unileon.es>
2021-06-15 10:16:05 +0200

    Modiuficnado el sql

--- app/db/ads.sql
+++ app/db/ads.sql
@@ -142,6 +142,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
+  `admin` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
